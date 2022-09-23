2fd3067ba3fb5be747de74c9c9ee90ef394aacd2
Guilherme Reis Araújo <guilherme.reis@memed.com.br>
2022-09-23 16:59:36 -0300

    remove unnecessary routes

--- src/api/routes/pets-routes.ts
+++ src/api/routes/pets-routes.ts
@@ -4,10 +4,6 @@ import PetsController from "../controllers/pets-controller";
 const petsRouter = Router();
 const petsController = new PetsController();
 
-petsRouter.get("/", (request, response) => {
-  return response.json("OK");
-});
-
 petsRouter.post("/", (request, response) => {
   return petsController.create(request, response);
 });
